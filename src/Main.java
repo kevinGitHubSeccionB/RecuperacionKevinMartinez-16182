de408756b326fbc93e59701da4fb6ae3407d0c38
kevinGitHubSeccionB <kmartinezc22@miumg.edu.gt>
2024-06-22 11:03:06 -0600

    tercer comentario KevinMartinez 0909-23-16182

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
         int option = 0;
         do {
             System.out.println("Menú:");
             System.out.println("1.Realizar operación +,-,*,/ ");
             System.out.println("2.");
-            System.out.println("3. ");
+            System.out.println("3.Ingresar un estudiante ");
             System.out.println("4. ");
             System.out.println("5. ");
             System.out.println("6. ");
@@ -28,7 +28,15 @@ public class Main {
                     System.out.println("Realizar operación +,-,*,/");
                     new ExamenFinalVarianteA();
                     break;
-
+                case 3:
+                    System.out.println("Opción 6: Ingresar un estudiante");
+                    System.out.print("Ingrese el nombre del estudiante: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Ingrese la calificación del estudiante: ");
+                    double calificacion = scanner.nextDouble();
+                    Estudiante estudiante = new Estudiante(nombre, calificacion);
+                    System.out.println("Estudiante ingresado: " + estudiante.getNombre() + " con calificación " + estudiante.getCalificacion());
+                    break;
             }
         }while (option != 9);
         scanner.close();
